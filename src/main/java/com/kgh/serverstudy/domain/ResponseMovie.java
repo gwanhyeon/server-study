48979bfafce73b5bcbf13e1b839151e564546125
gwanhyeon <kgh940525@gmail.com>
2021-01-16 16:53:43 +0900

    Add Naver Open search API

---
+++ src/main/java/com/kgh/serverstudy/domain/ResponseMovie.java
@@ -0,0 +1,31 @@
+package com.kgh.serverstudy.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseMovie {
+    private List<Item> items;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Item {
+
+        private String title;
+        private String link;
+        private String actor;
+        private String director;
+        private float userRating;
+
+        // todo field Adding
+    }
+}
--- src/main/java/com/kgh/serverstudy/service/MovieService.java
+++ src/main/java/com/kgh/serverstudy/service/MovieService.java
@@ -1,5 +1,6 @@
 package com.kgh.serverstudy.service;
 
+import com.kgh.serverstudy.Repository.MovieRepository;
 import com.kgh.serverstudy.domain.Movie;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,17 @@ import java.util.List;
 @Service
 public class MovieService {
 
-    public List<Movie> query(final String query){
+    private final MovieRepository movieRepository;
 
+    public MovieService(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+    public List<Movie> search(final String query){
+        return movieRepository.findByQuery(query);
+    }
+
+    // 로컬 임시 테스트
+    public List<Movie> query(final String query){
         return Arrays.asList(
                 Movie.builder().title("Movie-1").link("http://link").build(),
                 Movie.builder().title("Movie-2").link("http://link").build()
